ed6b56aa99f8f2fdbf1cc177f3c09bf7699c4a59
Tina Ngen <ngentina69@gmail.com>
2026-05-23 21:41:25 +0700

    null change

--- projects/I4S2-TP08-sales-analysis-project/src/loader.py
+++ projects/I4S2-TP08-sales-analysis-project/src/loader.py
@@ -2,17 +2,17 @@ from pathlib import Path
 
 from pyspark.sql import DataFrame, SparkSession
 
-
+# create spark session
 def create_spark_session(app_name: str = "SalesAnalysis") -> SparkSession:
     return (
         SparkSession.builder
         .appName(app_name)
-        .master("local[*]")
-        .config("spark.sql.shuffle.partitions", "8")
+        .master("local[*]") # use all available cores for local machine
+        .config("spark.sql.shuffle.partitions", "8") # limits shuffle partitions for small datasets
         .getOrCreate()
     )
 
-
+# load data
 def load_csv(spark: SparkSession, csv_path: str) -> DataFrame:
     path = Path(csv_path).resolve()
     if not path.exists():
